4d9d017d01643bef4514353f3ff9ecbd6398a3e5
alikon <optimus4joomla@gmail.com>
2023-04-08 08:39:33 +0200

    graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,10 @@ if uploaded_file is not None:
    
 year = [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
 unemployment_rate = [9.8, 12, 8, 7.2, 6.9, 7, 6.5, 6.2, 5.5, 6.3]
+data =df['execution'].tolist()
+openi=df['openi'].tolist()
   
-plt.plot(year, unemployment_rate, color='red', marker='o')
+plt.plot(data, openi, color='red', marker='o')
 plt.title('unemployment rate vs year', fontsize=14)
 plt.xlabel('year', fontsize=14)
 plt.ylabel('unemployment rate', fontsize=14)
