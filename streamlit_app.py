bace66e23870529befb3e496790c2840b97c4c72
alikon <optimus4joomla@gmail.com>
2023-04-09 09:05:20 +0200

    clean

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,10 @@ import streamlit as st
 from datetime import datetime
 import altair as alt
 
-def showIssues(df, ts):
-  # sorting data frame by a column
-  df.sort_values("id", axis=0, ascending=True,
-                 inplace=True, na_position='first')
-  newdf = df[(df.execution > ts)]
-
+def showIssues(df):
   source = pd.DataFrame({
-    'day': newdf['execution'].tolist(),
-    'issues': newdf['openi'].tolist()
+    'day': df['execution'].tolist(),
+    'issues': df['openi'].tolist()
   })
 
   chart = alt.Chart(source).mark_area(
@@ -31,8 +26,7 @@ def showIssues(df, ts):
   )
   st.altair_chart(chart)
 
-def showPulls(df, ts):
-
+def showPulls(df):
   source = pd.DataFrame({
     'day': df['execution'].tolist(),
     'pulls': df['openp'].tolist()
@@ -54,11 +48,8 @@ def showPulls(df, ts):
       alt.Y('pulls:Q',scale=alt.Scale(type='log', domain=[100, 350]),)
   )
   st.altair_chart(chart)
-
-st.title("Datasort")
-
-
-
+############################## start ##################################
+st.title("Joomla Github statistics")
 #uploaded_file = st.file_uploader("Choose a file")
 df = pd.read_csv("./data/dataset.csv")
 if df is not None:
@@ -69,12 +60,13 @@ if df is not None:
   # sorting data frame by a column
   df.sort_values("id", axis=0, ascending=True,
                  inplace=True, na_position='first')
+  # filter values
   newdf = df[(df.execution > ts)]
-
+  # show chart
   tab1, tab2 = st.tabs(["📈 Issues", "🗃 Pull Requests"])
   with tab1:
-    showIssues(df, ts)
+    showIssues(newdf)
   with tab2:
-    showPulls(newdf, ts)
-  with st.expander("See explanation"):
+    showPulls(newdf)
+  with st.expander("See data"):
     st.write(df)
